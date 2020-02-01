85273225e1063627edab2fdec160ffd3d40e7f3c
ArcecLightning <47046389+ArcecLightning@users.noreply.github.com>
2020-01-31 23:38:10 -0500

    Added some scripts for scrap and stuff

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/RoomScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomScript : MonoBehaviour
+{
+    public float maxHealth;
+    public float health;
+    private bool isActive = false;
+    //defaulted to false for start
+
+    private void Update()
+    {
+        if (health <= 0)
+        {
+            isActive = false;
+        }    
+    }
+
+
+    public void TakeDamage(int dmg)
+    {
+        health -= dmg;
+    }
+
+    public void addHealth(float health)
+    {
+        if (maxHealth < this.health + health)
+        {
+            this.health += health;
+        }
+        else
+        {
+            this.health = maxHealth;
+        }
+
+    }
+}
