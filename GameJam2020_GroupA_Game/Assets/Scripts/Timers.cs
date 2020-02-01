2da67dab80eff4f354da8ef94c167854b8c57fb8
Derek-Trump <58478075+Derek-Trump@users.noreply.github.com>
2020-02-01 08:06:04 -0500

    Timer
    Just added a timer it will count down but that is it right now it will not stop at 0 yet

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/Timers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timers : MonoBehaviour
+{
+    float currentTime = 0f;
+    public float startingTime = 120f;
+    [SerializeField] Text countdownText;
+    public void Start()
+    {
+        currentTime = startingTime;
+    }
+    public void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0.0");
+    }
+}
