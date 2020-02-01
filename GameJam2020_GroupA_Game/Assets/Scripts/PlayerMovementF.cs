1aff156d355561435c6c77e4057d533e999b3745
Alf-Fallon <49760422+Alf-Fallon@users.noreply.github.com>
2020-01-31 21:22:27 -0500

    Changed Unity Preferences

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/PlayerMovementF.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementF : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed;
+    private Rigidbody2D playerRig;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
