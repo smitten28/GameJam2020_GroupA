a8e23a52682b522f273bc8f72d7e238b13bd7bf1
Alf-Fallon <49760422+Alf-Fallon@users.noreply.github.com>
2020-01-31 22:07:41 -0500

    Testing Outside Movement Script

--- GameJam2020_GroupA_Game/Assets/Scripts/PlayerMovementF.cs
+++ GameJam2020_GroupA_Game/Assets/Scripts/PlayerMovementF.cs
@@ -6,18 +6,34 @@ public class PlayerMovementF : MonoBehaviour
 {
     [SerializeField]
     private float movementSpeed;
+
+    private float xAxis;
+    private float yAxis;
+    private float moveX;
+    private float moveY;
+
     private Rigidbody2D playerRig;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRig = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    void Movement()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        yAxis = Input.GetAxisRaw("Vertical");
+        moveX = xAxis * movementSpeed;
+        moveY = yAxis * movementSpeed;
+
+        playerRig.velocity = new Vector2(moveX, moveY);
     }
 }
