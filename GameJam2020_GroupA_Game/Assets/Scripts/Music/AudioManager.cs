0a0ea092f944d490df19e3a493ec70bc05b3bdaf
Ashley <57230143+smitten28@users.noreply.github.com>
2020-02-01 10:21:54 -0500

    General Updates
    1. Added Audio Manager
    2. Added Footstep script
    3. General cleanup in scripts

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/Music/AudioManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    private void Awake()
+    {
+       DontDestroyOnLoad(this);
+       foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+            s.source.outputAudioMixerGroup = s.mixer;
+        }
+       
+    }
+
+    public void play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.Play();
+    }
+    public void stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+    }
+    public void playOneShot(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if(s == null)
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return;
+        }
+        s.source.PlayOneShot(s.clip);
+    }
+}
---
+++ GameJam2020_GroupA_Game/Assets/Scripts/Music/FootSteps.cs
@@ -0,0 +1,31 @@
+﻿//==========================================================================================
+//Author: Ashley Angel
+//Date  : 02/01/2020
+//Desc  : plays random clips for footstep sounds
+//==========================================================================================
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] clips;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void step()
+    {
+        AudioClip clip = getRandomClip();
+        audioSource.PlayOneShot(clip);
+    }
+    private AudioClip getRandomClip()
+    {
+        return clips[UnityEngine.Random.Range(0, clips.Length)];
+    }
+
+}
