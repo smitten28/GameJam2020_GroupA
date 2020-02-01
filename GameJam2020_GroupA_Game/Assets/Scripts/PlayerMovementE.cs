c2fe3a78af0ec5ac006c28a5bc4c10ef7493bd8f
Ethanopia <60530052+Ethanopia@users.noreply.github.com>
2020-01-31 21:23:49 -0500

    Created personal testing scene

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/PlayerMovementE.cs
@@ -0,0 +1,30 @@
+﻿// =======================================================================
+// Name: Ethan Harbaugh
+// Date: 01/31/2020
+// Desc: Ethan's personal test script  NOT FOR USE IN THE FINAL VERSION
+// =======================================================================
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementE : MonoBehaviour
+{
+
+    [SerializedField]
+    private float movementSpeed;
+    private Ridgidbody2D 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
