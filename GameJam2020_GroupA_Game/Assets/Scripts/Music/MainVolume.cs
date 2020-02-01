5c3d4211c544812575d1ae2f88534eb1fa4baf6c
Ashley <57230143+smitten28@users.noreply.github.com>
2020-02-01 13:56:08 -0500

    Menu changes

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/Music/MainVolume.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainVolume : MonoBehaviour
+{
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] Slider slider;
+    private float sliderValue;
+
+    public void Start()
+    {
+        FindObjectOfType<AudioManager>().play("InsideBackgroundMusic");
+    }
+    public void setVolume()
+    {
+        sliderValue = slider.value;
+        mixer.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+    }
+
+}
--- GameJam2020_GroupA_Game/Assets/Scripts/Music/Sound.cs
+++ GameJam2020_GroupA_Game/Assets/Scripts/Music/Sound.cs
@@ -13,8 +13,6 @@ public class Sound
 
     [Range(0f, 1f)]
     public float volume;
-    [Range(.1f, 3f)]
-    public float pitch;
 
     public bool loop;
 
