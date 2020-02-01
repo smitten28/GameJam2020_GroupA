1d2b2ac7c952752fe7339312ca0ff64393ed0aea
Ashley <57230143+smitten28@users.noreply.github.com>
2020-02-01 02:36:23 -0500

    simple Ship layout

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused;
+    public GameObject pauseMenuUI;
+    public GameObject gameUI;
+
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+        gameIsPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(!gameIsPaused)
+            {
+                pause();
+            }
+            else
+            {
+                resume();
+            }
+        }
+    }
+    public void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        gameUI.SetActive(true);
+        gameIsPaused = true;
+        Time.timeScale = 0f;
+    }
+    public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        gameUI.SetActive(true);
+        gameIsPaused = false;
+        Time.timeScale = 1f;
+
+    }
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+    public bool getGameIsPaused()
+    {
+        return gameIsPaused;
+    }
+}
