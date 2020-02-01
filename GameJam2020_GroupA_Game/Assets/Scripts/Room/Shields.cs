eb65bc616b54c929dbe3ac8040655bbcf9ab5af5
ArcecLightning <47046389+ArcecLightning@users.noreply.github.com>
2020-02-01 17:42:17 -0500

    added shields

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/Room/Shields.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shields : MonoBehaviour
+{
+    private float shieldHealth;
+    private bool isActive;
+
+    public void takeDamage(float a)
+    {
+        if (shieldHealth - a <= 0)
+        {
+            shieldHealth = 0;
+            isActive = false;
+        }
+    }
+    public void addHealth(float a)
+    {
+        if (shieldHealth <= 0)
+        {
+            isActive = true;
+        }
+        shieldHealth += a;
+    }
+    public void updateShieldHealth()
+    {
+
+    }
+}
