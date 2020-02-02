cc45cb6cd663b1af53909fa94c6cbffc48b58a31
Arcec <Arceclightning@gmail.com>
2020-02-02 14:12:31 -0500

    added outside timer

--- GameJam2020_GroupA_Game/Assets/Scripts/OutsideShip/OutsideTimer.cs
+++ GameJam2020_GroupA_Game/Assets/Scripts/OutsideShip/OutsideTimer.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OutsideTimer : MonoBehaviour
 {
+    public float maxTime;
+    public float curTimeRemaining;
+
+    public Text timerText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +20,20 @@ public class OutsideTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        calcTime();
+        if (curTimeRemaining <= 0)
+        {
+            //timer is up
+            SceneManager.LoadScene("MainShip");
+        }
     }
 
-
+    private void calcTime()
+    {
+        curTimeRemaining -= Time.deltaTime;
+    }
+    private void updateText()
+    {
+        timerText.text = curTimeRemaining.ToString();
+    }
 }
