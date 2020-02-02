df695ac51fc05866cda2dac28534840b26626466
Arcec <Arceclightning@gmail.com>
2020-02-02 12:34:14 -0500

    edited waves

--- GameJam2020_GroupA_Game/Assets/Scripts/Enemy/Waves.cs
+++ GameJam2020_GroupA_Game/Assets/Scripts/Enemy/Waves.cs
@@ -6,12 +6,17 @@ using UnityEngine.UI;
 public class Waves : MonoBehaviour
 {
     public int maxWaves;
-    public int curWave;
+    static public int curWave;
     public float maxTimePerWave;
     public float waveTime;
     public float maxTimeInBetweenWave;
     public float timeBetweenWave;
     public float maxEnemies;
+    public GameObject enemy1Prefab;
+    public GameObject enemy2Prefab;
+
+    [SerializeField]
+    private Vector2[] range;
 
     [SerializeField]
     private Text waveText;
@@ -41,7 +46,27 @@ public class Waves : MonoBehaviour
 
     public void spawnEnemy()
     {
-         //start spawning enemies
+        //start spawning enemies
+        float randX;
+        float randY;
+
+        for (int i = 0; i < maxEnemies * curWave / 2; i++)
+        {
+            //max * cur wave so we have scaling amounts of enemies
+            randX = Random.Range(range[0].x, range[1].x);
+            randY = Random.Range(range[0].y, range[1].y);
+            if (Random.Range(0, 1) == 0)
+            {
+                //spawn enemy1
+                Instantiate(enemy1Prefab, new Vector3(randX, randY, 0), Quaternion.identity);
+            }
+            else
+            {
+                //spawn enemy2
+                Instantiate(enemy2Prefab, new Vector3(randX, randY, 0), Quaternion.identity);
+            }
+
+        }
     }
 
     private void updateText()
