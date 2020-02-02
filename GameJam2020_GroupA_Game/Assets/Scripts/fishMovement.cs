ad7b0ca8759fb6c2627aa1042cea7c7ed9df2481
Zackary-C-Moore <45342911+Zackary-C-Moore@users.noreply.github.com>
2020-02-01 23:02:47 -0500

    Fish Moving in tank - ZM
    fish moving in tank: added fish prefab, fish movement script, and walls for the fish tank.

---
+++ GameJam2020_GroupA_Game/Assets/Scripts/fishMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fishMovement : MonoBehaviour
+{
+    Rigidbody2D fishRb;
+    //set to negative if facing left.
+    public float moveSpeed;
+    void Start()
+    {
+        fishRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fishRb.velocity = new Vector2(moveSpeed, fishRb.velocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("FishTankLeftWall"))
+        {
+            moveSpeed *= -1;
+            //need to do this becuase the fish have an animation
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+        }
+        else if(collision.gameObject.CompareTag("FishTankRightWall"))
+        {
+            moveSpeed *= -1;
+            //need to do this becuase the fish have an animation
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        }
+    }
+}
